bc679e097067d7fddc7fbf6bcd8ae7c64dab9515
feri <rht.farideh@yahoo.com>
2023-04-26 12:45:56 +0200

    add subtract function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {multiply , add, } from './calculation.jsx'
+import {multiply , add,subtract } from './calculation.jsx'
 
 function App() {
   return(
